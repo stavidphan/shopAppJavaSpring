558230e1d2ac06b9699beaba933eac6a51bde4c2
StavidPhan <thanhduyphan2123@gmail.com>
2024-08-12 17:03:37 +0700

    add CRUD for orders

---
+++ src/main/java/com/project/shopapp/controllers/OrderController.java
@@ -0,0 +1,62 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.OrderDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/orders")
+public class OrderController {
+    @PostMapping("")
+    public ResponseEntity<?> createOrder(@Valid @RequestBody OrderDTO orderDTO, BindingResult result) {
+        try {
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+
+            return ResponseEntity.ok("create order successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<?> getOrders(@Valid @PathVariable("user_id") Long userId) {
+        try {
+            // lay danh sach order cua user co id = user_id
+            return ResponseEntity.ok("get orders by user id: " + userId);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    // Công việc của admin
+    public ResponseEntity<?> updateOrder(@Valid @PathVariable Long id, @Valid @RequestBody OrderDTO orderDTO) {
+        try {
+            // update order co id = id
+            return ResponseEntity.ok("update order with id: " + id);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteOrder(@Valid @PathVariable Long id) {
+        try {
+            // xoá mềm => cập nhật  trường active
+            return ResponseEntity.ok("delete order with id: " + id);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/project/shopapp/dtos/OrderDTO.java
@@ -0,0 +1,46 @@
+package com.project.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+    @JsonProperty("user_id")
+    @Min(value = 1, message = "User ID must be > 0")
+    private Long userId;
+
+    @JsonProperty("fullname")
+    private String fullName;
+
+    private String email;
+
+    @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 10, max = 10, message = "Phone number must be 10 characters")
+    private String phoneNumber;
+
+    private String address;
+
+    private String note;
+
+    @JsonProperty("total_money")
+    @Min(value = 0, message = "Total money must be >= 0")
+    private Float totalMoney;
+
+    @JsonProperty("shipping_method")
+    private String shippingMethod;
+
+    @JsonProperty("shipping_address")
+    private String shippingAddress;
+
+    @JsonProperty("payment_method")
+    private String paymentMethod;
+}
